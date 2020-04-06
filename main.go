365d206766386c9920f3253b8bbb02bdcbd73233
saurav <saurav.singh@zomato.com>
2020-04-06 09:54:56 +0530

    path fix

--- main.go
+++ main.go
@@ -10,13 +10,14 @@ func main() {
 	engine := gin.Default()
 	engine.LoadHTMLGlob("templates/*")
 	engine.Static("/static","./static")
+	engine.RedirectFixedPath = true
 	engine.GET("/", func(context *gin.Context) {
-		coronaUpdate := external.LiveData()
+		coronaUpdate := external.CrowdData()
 		context.HTML(
 			// Set the HTTP status to 200 (OK)
 			http.StatusOK,
 			// Use the index.html template
-			"index.html",
+			"crowdsource.html",
 			// Pass the data that the page uses (in this case, 'title')
 			gin.H{
 				"title": "COVID-19",
@@ -25,12 +26,16 @@ func main() {
 		)
 	})
 	engine.GET("/crowdsourced", func(context *gin.Context) {
-		coronaUpdate := external.CrowdData()
+		context.Redirect(http.StatusMovedPermanently, "/")
+		context.Abort()
+	})
+	engine.GET("/moh", func(context *gin.Context) {
+		coronaUpdate := external.LiveData()
 		context.HTML(
 			// Set the HTTP status to 200 (OK)
 			http.StatusOK,
 			// Use the index.html template
-			"crowdsource.html",
+			"index.html",
 			// Pass the data that the page uses (in this case, 'title')
 			gin.H{
 				"title": "COVID-19",
